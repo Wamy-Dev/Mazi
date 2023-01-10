f72114f7c020e4f200e0f591599d037cec55f024
lpjhelder <83678766+lpjhelder@users.noreply.github.com>
2023-01-09 23:39:34 -0300

    Update donate.py

--- cogs/donate.py
+++ cogs/donate.py
@@ -8,10 +8,10 @@ class Donate(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @app_commands.command(name="donate", description="Doante to the Mazi project.")
+    @app_commands.command(name="donate", description="Doe ao projeto Mazi.")
     async def donate(self, interaction: Interaction):
 
-        embed = discord.Embed(title = "Donate to the project", colour = discord.Colour.from_rgb(229,160,13))
+        embed = discord.Embed(title = "Doe para o projeto", colour = discord.Colour.from_rgb(229,160,13))
         embed.set_author(name = interaction.user.display_name, icon_url = interaction.user.display_avatar.url)
         embed.add_field(name = '🔗', value='https://homeonacloud.com/donate', inline = False)
         await interaction.response.send_message(embed=embed)
